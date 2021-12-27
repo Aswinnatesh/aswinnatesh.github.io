5820bba16e6f914ff278e02385615d7d6cea4f54
Aswin's MAC <aswinnatesh.venkatesh@stonybrook.edu>
2021-12-27 08:26:56 -0800

    Adding a script to create a post directory

---
+++ scripts/CreatePostContainer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import os, sys
+import shutil
+import argparse
+
+def main():
+    try:
+        parser = argparse.ArgumentParser(epilog='v{}')
+        parser.add_argument('-dir', required=True, help='help text for default')
+        args = parser.parse_args()
+
+    except Exception as e:
+        print(e)
+
+    out_dir = f"./{args.dir}"
+    print(f"Creating Directory {out_dir}!")
+    if os.path.isdir(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
+    os.chdir(out_dir)   
+
+    title = {args.dir}
+    side_bar_name = (args.dir).replace("-", " ")
+    side_bar_url = (args.dir).lower()
+    f= open("./_index.md","w+")
+    f.write (f"--- \ntitle: {title} \nmenu:\n\tsidebar:\n\t\tname: {side_bar_name}\n\t\tidentifier: {side_bar_url}\n\t\tweight: 300\n---")
+    f.close()
+
+if __name__ == '__main__':
+    main()
